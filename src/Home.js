bc333c1357674a9ce4902a4906710d085c2ee54b
LeeYoungMin <miniyoung37@gmail.com>
2019-11-13 01:25:31 +0900

    react-apollo & queries

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({ loading, data, error }) => {
+      if (loading) return "loading";
+      if (error) return "something happened";
+      if (data) {
+        console.log(data);
+        return data.movies.map(movie => (
+          <h2 key={movie.id}>
+            {movie.title} / {movie.rating}
+          </h2>
+        ));
+      }
+    }}
+  </Query>
+);
 
 export default Home;
