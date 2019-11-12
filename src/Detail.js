dc30d96f7b7a76a4e682e2b17c56fce1312cd55d
LeeYoungMin <miniyoung37@gmail.com>
2019-11-13 02:53:40 +0900

    update detail page

--- src/Detail.js
+++ src/Detail.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { Query } from "react-apollo";
+import { MOVIE_DETAIL } from "./queries";
 
 const Detail = ({
   match: {
     params: { movieId }
   }
-}) => {
-  console.log(movieId);
-  return <div>Detail</div>;
-};
+}) => (
+  <Query query={MOVIE_DETAIL} variables={{ movieId }}>
+    {({ loading, error, data }) => {
+      if (loading) return "loading";
+      if (error) return "error";
+      return (
+        <React.Fragment>
+          {data.movie.title}
+          {data.movie.medium_cover_image}
+          {data.movie.rating}
+          {data.movie.description_intro}
+        </React.Fragment>
+      );
+    }}
+  </Query>
+);
 
 export default Detail;
--- src/queries.js
+++ src/queries.js
@@ -11,4 +11,20 @@ export const HOME_PAGE = gql`
   }
 `;
 
-// export const MOVIE_DETAIL = gql``;
+export const MOVIE_DETAIL = gql`
+  query getMovieDetails($movieId: Int!) {
+    movie(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+      description_intro
+      language
+      genres
+    }
+    suggestions(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+    }
+  }
+`;
